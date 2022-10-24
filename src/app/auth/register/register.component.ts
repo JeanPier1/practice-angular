8427eb6082a0894dea8c8b52e5146ee0d3dd8221
JeanPier1 <jeangonza@upeu.edu.pe>
2022-10-24 14:36:46 -0500

    config-practice-angular

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-register',
@@ -7,17 +8,22 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  public registerForm = this.fb.group({
-    nombre: ['', Validators.required],
-    email: ['', [Validators.required,Validators.email]],
-    password: ['', Validators.required],
-    password2: ['', Validators.required],
-    terminos: [false, Validators.required],
-  });
+  public registerForm = this.fb.group(
+    {
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+      terminos: [false, Validators.required],
+    },
+    {
+      Validators: this.passwordsIguales('password', 'password2'),
+    }
+  );
 
   public formSubmitted = false;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private service: UsuarioService) {}
 
   ngOnInit(): void {}
 
@@ -25,11 +31,17 @@ export class RegisterComponent implements OnInit {
     this.formSubmitted = true;
 
     if (this.registerForm.value) {
-      console.log("posteando formulario");
-      
+      console.log('posteando formulario');
+
+      this.service.crearUsuario(this.registerForm.value).subscribe(
+        (resp) => {
+          console.log('Usuario creado');
+          console.log(resp);
+        },
+        (err) => console.warn(err)
+      );
     } else {
-      console.log("formulario no es correcto....");
-      
+      console.log('formulario no es correcto....');
     }
 
     console.log(this.registerForm.value);
@@ -43,8 +55,25 @@ export class RegisterComponent implements OnInit {
     }
   }
 
-  aceptaTerminos(){
+  contrasenasValidas() {
+    const pass1 = this.registerForm.get('password')?.value;
+
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    return pass1 !== pass2 && this.formSubmitted ? true : false;
+  }
+
+  aceptaTerminos() {
     return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
 
+  passwordsIguales(pass1Name: string, pass2Name: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+      return pass1Control === pass2Control
+        ? pass2Control?.setErrors(null)
+        : pass2Control?.setErrors({ noEsIgual: true });
+    };
+  }
 }
