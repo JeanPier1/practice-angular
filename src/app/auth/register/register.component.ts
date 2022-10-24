794ef8df0ea8134f1902913e3fa4a1809f7e59bc
jeanpier1 <jeangonza@upeu.edu.pe>
2022-10-23 21:58:06 -0500

    login 1

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public registerForm = this.fb.group({
+    nombre: ['', Validators.required],
+    email: ['', [Validators.required,Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terminos: [false, Validators.required],
+  });
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  createuser() {
+    this.formSubmitted = true;
+
+    if (this.registerForm.value) {
+      console.log("posteando formulario");
+      
+    } else {
+      console.log("formulario no es correcto....");
+      
+    }
+
+    console.log(this.registerForm.value);
+  }
+
+  campoNoValido(campo: string): boolean {
+    if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
 
 }
